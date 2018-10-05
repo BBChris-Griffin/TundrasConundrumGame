c78217004d936b63784996a016c4f99fc46e1ace
Chris Griffin <32560724+BBChris-Griffin@users.noreply.github.com>
2018-10-04 23:01:15 -0700

    Additional Refinement

--- TundraConundrum/Assets/Scripts/AdventureGame.cs
+++ TundraConundrum/Assets/Scripts/AdventureGame.cs
@@ -11,6 +11,8 @@ public class AdventureGame : MonoBehaviour {
     public GameObject answerButton;
     public GameObject startPoint;
     public float buttonOffset;
+    public float initTransButtonOffset;
+
 
     private State currState;
     private List<GameObject> buttons;
@@ -42,10 +44,16 @@ public class AdventureGame : MonoBehaviour {
             buttons.Clear();
         }
 
+        float startPos = 0.0f;
+        if(currState.GetAnswers().Length == 2)
+        {
+            startPos = initTransButtonOffset;
+        }
+
         for (int i = 0; i < currState.GetAnswers().Length; i++)
         {
             GameObject answer = Instantiate(answerButton, startPoint.transform).gameObject;
-            answer.transform.position = new Vector3(startPoint.transform.position.x + (buttonOffset * i) / currState.GetAnswers().Length, 
+            answer.transform.position = new Vector3(startPos + (startPoint.transform.position.x + (buttonOffset * i) / currState.GetAnswers().Length), 
                 startPoint.transform.position.y, startPoint.transform.position.z);
             answer.GetComponentInChildren<Text>().text = currState.GetAnswers()[i];
             buttons.Add(answer);
