e9fbd07e216fc358972a43f0252169e00c8d02da
Chris Griffin <32560724+BBChris-Griffin@users.noreply.github.com>
2018-10-29 12:12:49 -0700

    Adding Room Creation
    Work on progress

---
+++ TundraConundrum/Assets/CreateRoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRoom : MonoBehaviour {
+
+	public GameObject path;
+	public State failState;
+	public State victoryState;
+	private State startState;
+	private AdventureGame game;
+	// Use this for initialization
+	void Start () {
+		gameController = FindGameObjectWithTag("GameController");
+		if(gameController != null)
+		{
+			game = gameController.GetComponent<AdventureGame>();
+		}
+		startState = game.startingState;
+	}
+
+	void CreateRoom()
+	{
+		State currState = startState;
+		
+	}
+}
